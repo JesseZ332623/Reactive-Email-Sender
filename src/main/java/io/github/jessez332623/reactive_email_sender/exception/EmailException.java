ff1b6abfc3f0da62c10bc0dd033cb22b005ef16b
浩进 朱 <3191955858@qq.com>
2025-09-25 17:29:04 +0800

    邮件发送异常类。

---
+++ src/main/java/io/github/jessez332623/reactive_email_sender/exception/EmailException.java
@@ -0,0 +1,59 @@
+package io.github.jessez332623.reactive_email_sender.exception;
+
+import lombok.Getter;
+
+/** 邮件发送异常类。*/
+@Getter
+public class EmailException extends RuntimeException
+{
+    /**
+     * 邮件发送过程中会出现很多原因导致的错误，
+     * 大致分为下面 4 类：
+     */
+    public enum ErrorType
+    {
+        /** 邮箱服务授权码错误。 */
+        AUTH_FAILURE,
+
+        /** 网络错误。 */
+        NETWORK_ISSUE,
+
+        /** 错误的邮箱地址格式。 */
+        INVALID_CONTENT,
+
+        /** 配置属性错误。 */
+        CONFIG_MISSING,
+
+        /** 附件不存在错误。*/
+        ATTACHMENT_NOT_EXIST
+    }
+
+    private final ErrorType errorType;
+
+    /**
+     * 邮件发送异常的构造。
+     *
+     * @param errorType     错误类型
+     * @param message       异常信息
+     * @param cause         异常本体
+     */
+    public EmailException(
+        ErrorType errorType, String message, Throwable cause)
+    {
+        super(message, cause);
+        this.errorType = errorType;
+    }
+
+    /**
+     * 邮件发送异常的构造。
+     *
+     * @param errorType     错误类型
+     * @param message       异常信息
+     */
+    public EmailException(
+        ErrorType errorType, String message)
+    {
+        super(message);
+        this.errorType = errorType;
+    }
+}
