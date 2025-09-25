e53f366115612241eecc98dee459c5a62127e1b3
浩进 朱 <3191955858@qq.com>
2025-09-25 17:46:49 +0800

    封装了 javax.mail 库的响应式邮件发送器接口。

---
+++ src/main/java/io/github/jessez332623/reactive_email_sender/ReactiveEmailSender.java
@@ -0,0 +1,23 @@
+package io.github.jessez332623.reactive_email_sender;
+
+
+import io.github.jessez332623.reactive_email_sender.dto.EmailContent;
+import io.github.jessez332623.reactive_email_sender.exception.EmailException;
+import org.jetbrains.annotations.NotNull;
+import reactor.core.publisher.Mono;
+
+/** 封装了 javax.mail 库的响应式邮件发送器接口。*/
+public interface ReactiveEmailSender
+{
+    /**
+     * 外部可调用的发送邮件的方法。
+     *
+     * @param emailContent 邮件内容
+     *
+     * @throws EmailException 当发送邮件失败时抛出
+     *
+     * @return 表示操作是否正确完成的 {@link Mono}
+     */
+    Mono<Void>
+    sendEmail(@NotNull EmailContent emailContent);
+}
