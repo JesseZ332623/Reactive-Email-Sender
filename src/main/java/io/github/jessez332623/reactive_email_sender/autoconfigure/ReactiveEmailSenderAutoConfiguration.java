9269f9fdbc4fadfcd020d365d763eefe7373cb5f
浩进 朱 <3191955858@qq.com>
2025-09-26 10:02:14 +0800

    响应式邮件发送器 Spring 自动配置类。

--- src/main/java/io/github/jessez332623/reactive_email_sender/autoconfigure/ReactiveEmailSenderAutoConfiguration.java
+++ src/main/java/io/github/jessez332623/reactive_email_sender/autoconfigure/ReactiveEmailSenderAutoConfiguration.java
@@ -1,6 +1,7 @@
 package io.github.jessez332623.reactive_email_sender.autoconfigure;
 
 import io.github.jessez332623.reactive_email_sender.ReactiveEmailSender;
+    import io.github.jessez332623.reactive_email_sender.authorization.EmailServiceAuthCodeGetter;
 import io.github.jessez332623.reactive_email_sender.exception.EmailException.ErrorType;
 import io.github.jessez332623.reactive_email_sender.exception.EmailException;
 import io.github.jessez332623.reactive_email_sender.impl.DefaultReactiveEmailSenderImpl;
@@ -14,6 +15,8 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 /** 响应式邮件发送器 Spring 自动配置类。*/
@@ -29,6 +32,9 @@ public class ReactiveEmailSenderAutoConfiguration
 {
     @Autowired
     private Validator validator;
+
+    @Autowired
+    private EmailServiceAuthCodeGetter authCodeGetter;
 
     @Autowired
     private ReactiveEmailSenderProperties properties;
@@ -64,6 +70,30 @@ public class ReactiveEmailSenderAutoConfiguration
         }
     }
 
+    /**
+     * 获取邮箱授权码
+     *（优先级：用户 {@link EmailServiceAuthCodeGetter} 接口实现 -> 用户配置文件）
+     *
+     * @throws EmailException 若在所有渠道都拿不到授权码时抛出
+     */
+    private String getAuthCode()
+    {
+        final String authCode
+            = Optional.ofNullable(this.authCodeGetter.get())
+                      .orElse(this.properties.getAuthCode());
+
+        if (Objects.isNull(authCode))
+        {
+            throw new
+            EmailException(
+                ErrorType.CONFIG_MISSING,
+                "Property <auth-code> is missing..."
+            );
+        }
+
+        return authCode;
+    }
+
     /** 响应式邮件发送器自动装配方法。*/
     @Bean
     @ConditionalOnMissingBean(value = {DefaultReactiveEmailSenderImpl.class})
@@ -76,7 +106,7 @@ public class ReactiveEmailSenderAutoConfiguration
             .maxAttemptTimes(this.properties.getMaxAttemptTimes())
             .maxAttachmentSize(this.properties.getMaxAttachmentSize())
             .enterPriceEmailAddress(this.properties.getSenderEmail())
-            .serviceAuthCode(this.properties.getAuthCode())
+            .serviceAuthCode(this.getAuthCode())
             .setDefaultSessionProperties()
             .addProperties(this.properties.getSessionProps())
             .build();
